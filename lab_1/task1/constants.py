5da495c88c2f52ff633d921a61f9a3b15b4a3834
Pyshisha <trollmisha2@gmail.com>
2025-06-03 12:33:04 +0300

    удалил lab1, lab2, lab4

--- lab_1/task1/constants.py
+++
@@ -1,9 +0,0 @@
-ALPHABET = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
-
-ORIGINAL_TEXT = "original.txt"
-
-KEY = "key.txt"
-
-ENCRYPT_TEXT = "encrypted.txt"
-
-ALPHABET_SIZE = len(ALPHABET)
--- lab_1/task2/constants.py
+++
@@ -1,7 +0,0 @@
-ENCRYPTED_TEXT = "encrypted.txt"
-
-DECRYPTED_TEXT = "decrypted.txt"
-
-KEY = "key.json"
-
-RUSSIAN_FREQUENCIES = "frequency.json"
